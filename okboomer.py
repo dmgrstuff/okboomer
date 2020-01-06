6ec328a3924548173dcf4905ec11a80f0e1244ed
dmgrstuff <54510230+dmgrstuff@users.noreply.github.com>
2020-01-05 22:52:15 -0500

    uploading okboomer.py
    this was a mistake

---
+++ okboomer.py
@@ -0,0 +1,21 @@
+boomer_years = list(range(1946, 1965))
+genx_years = list(range(1965, 1981))
+millennial_years = list(range(1981, 1997))
+genz_years = (list(range(1997, 2010)))
+genalpha_years = (list(range(2011, 2026)))
+allyears = (list(range(1946, 2026)))
+
+year = (int(input("What year were you born? ")))
+if year in boomer_years:
+    print("Okay, boomer.")
+elif year in genx_years:
+    print ("I literally don't know anything about Gen X so I don't know what to put here lol")
+elif year in millennial_years:
+    print("What industry are you ruining now?")
+elif year in genz_years:
+    print("So you're responsible for \"Okay, boomer\"?")
+elif year in genalpha_years:
+    print("[insert something negative about gen alpha here]")
+else:
+    print("uh idk lol")
+    
